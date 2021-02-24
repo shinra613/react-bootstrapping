827905e0d86d22a8b2bc6e27752cd66174ae2317
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2021-02-25 00:55:37 +0530

    new code initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import user from './user';
+
+function App() {
+  return (
+    <div className="App">
+     
+        {user({
+          name: "Akhil",
+          role:"father"
+        })}
+      {user({
+          name: "Aiswarya",
+          role:"mother"
+      })}
+      {user({
+          name: "Shinra",
+          role:"son"
+        }) }
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/user.js
@@ -0,0 +1,8 @@
+export default function user ({ name, role }) {
+
+  return (<div className="details">
+    <h1>{name.toUpperCase()}</h1>
+    <h2>{role}</h2>
+  </div>);
+
+};
