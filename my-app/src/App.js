0c54a8df87f1b65ff95ada50a165d42bd02a179f
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2021-03-05 21:49:34 +0530

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import user from './user';
+import logo from './logo.svg'
+import React from 'react'
+import './App.css'
+import user from './user'
 
 function App() {
   return (
     <div className="App">
      
-        {user({
-          name: "Akhil",
-          role:"father"
-        })}
       {user({
-          name: "Aiswarya",
-          role:"mother"
+        name: "Akhil",
+        role: "father"
       })}
       {user({
-          name: "Shinra",
-          role:"son"
-        }) }
+        name: "Aiswarya",
+        role: "mother"
+      })}
+      {user({
+        name: "Shinra",
+        role: "son"
+      })}
       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- my-app/src/user.js
+++ my-app/src/user.js
@@ -1,8 +1,11 @@
-export default function user ({ name, role }) {
+import React from "react"
+
+
+export default function user({ name, role }) {
 
   return (<div className="details">
     <h1>{name.toUpperCase()}</h1>
     <h2>{role}</h2>
-  </div>);
+  </div>)
 
-};
+}
